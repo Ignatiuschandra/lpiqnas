abddefa1b5c58d630eb0abddcc6e7bbb81ebda19
Ignatius Chandra <ignasiosc@gmail.com>
2020-01-06 10:13:09 +0700

    update check token dan flare

--- app/Http/Middleware/CheckToken.php
+++ app/Http/Middleware/CheckToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Request;
 use Closure;
 use App\Models\Siswa;
 
@@ -14,7 +15,7 @@ class CheckToken
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $token = $request->header('Authorization');
         if(empty($token)){
