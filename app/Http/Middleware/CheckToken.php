088460ae44dd3ac8ca527e042fac2f4cbe526679
Ignatius Chandra <ignasiosc@gmail.com>
2020-01-12 11:55:59 +0700

    update cara auth token

--- app/Http/Middleware/CheckToken.php
+++ app/Http/Middleware/CheckToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
+use Concerns\InteractsWithInput;
 use Closure;
 use App\Models\Siswa;
 
@@ -17,29 +18,33 @@ class CheckToken
      */
     public function handle(Request $request, Closure $next)
     {
-        $token = $request->header('Authorization');
+        $token = $request->header('lpiqnas-api-key');
+        // $headers = getallheaders();
         if(empty($token)){
             return response()->json([
-                'error' => 'Authorization Header is empty'
+                'error' => 'Authorization Header is empty',
+                // 'requestPHP' => $token,
+                // 'requestLaravel' => $headers
             ]);
         }
 
-        //format bearer token : 
-        //Bearer[spasi]randomhashtoken 
-        $pecah_token = explode(" ", $token);
-        if(count($pecah_token) <> 2){
-            return response()->json([
-                'error' => 'Invalid Authorization format'
-            ]);
-        }
+        // //format bearer token : 
+        // //Bearer[spasi]randomhashtoken 
+        // $pecah_token = explode(" ", $token);
+        // if(count($pecah_token) <> 2){
+        //     return response()->json([
+        //         'error' => 'Invalid Authorization format'
+        //     ]);
+        // }
 
-        if(trim($pecah_token[0]) <> 'Bearer'){
-            return response()->json([
-                'error' => 'Authorization header must be a Bearer'
-            ]);
-        }
+        // if(trim($pecah_token[0]) <> 'Bearer'){
+        //     return response()->json([
+        //         'error' => 'Authorization header must be a Bearer'
+        //     ]);
+        // }
 
-        $access_token = trim($pecah_token[1]);
+        // $access_token = trim($pecah_token[1]);
+        $access_token = $token;
 
         //cek apakah access_token ini ada di database atau tidak
         $cek = Siswa::where('siswa_access_token', $access_token)->first();
